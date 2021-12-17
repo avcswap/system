1f901b7d23de311521d71835887301972d20844d
avcswap <94029661+avcswap@users.noreply.github.com>
2021-12-17 13:36:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 
-//'<script src="https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js"></script>'
+document.write('<script src="https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js"></script>')
+
