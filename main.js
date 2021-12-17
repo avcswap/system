48e8a2799903c343cf500a84d4d4c9cdc98bc321
avcswap <94029661+avcswap@users.noreply.github.com>
2021-12-17 17:17:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 
-document.write(`<script src="https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js"></script>`)
-var x = document.createElement("P");                        // Create a <p> element
-var t = document.createTextNode("This is a paragraph.");    // Create a text node
-x.appendChild(t);                                           // Append the text to <p>
-document.body.appendChild(x); 
 
 console.log('runing')
 
